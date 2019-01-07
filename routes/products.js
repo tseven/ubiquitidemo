7e509eecca242d1175421c47b641f6db5c266aa6
Steven Kinley <steven@pasthem.com>
2019-01-07 12:13:08 -0800

    Modified Products router and view, added request error handling

---
+++ routes/products.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+var rp = require('request-promise-native');
+
+var options = {
+    uri: 'https://amplifi-dev.myshopify.com/products.json',
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    json: true // Automatically parses the JSON string in the response
+};
+
+rp(options)
+    .then(function (jsonData) {
+        console.log('User has %d products', jsonData.products.length);
+        /* GET home page. */
+        router.get('/', function(req, res, next) {
+            res.render('products', { title: 'Express' });
+        });
+
+
+
+    })
+    .catch(function (err) {
+        // API call failed...
+        router.get('/', function(req, res, next) {
+            // var loadError = new Error("Doh, couldn't load the json");
+            // console.log(loadError);
+            console.log(err);
+
+            res.render('error', err);
+        });
+    });
+
+module.exports = router;
